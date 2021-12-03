65227a7c0e3bd32d56e989eed21b0b3547cfb302
Gabriel RIGO <grigo@e2r11p14.clusters.42paris.fr>
2021-12-03 18:50:24 +0100

    finish Grace

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-// ceci est un commentaire
+/*
+ceci est un commentaire
+*/
 void fct(){
-	char *a = "#include <stdio.h>%1$c// ceci est un commentaire%1$cvoid fct(){%1$c%2$cchar *a = %3$c%4$s%3$c;printf(a, 10, 9, 34, a);%1$c}%1$cint main(){%1$c%2$c//le deuxieme commentaire%1$c%2$cfct();%1$c}%1$c";printf(a, 10, 9, 34, a);
+	char *a = "#include <stdio.h>%1$c/*%1$cceci est un commentaire%1$c*/%1$cvoid fct(){%1$c%2$cchar *a = %3$c%4$s%3$c;printf(a, 10, 9, 34, a);%1$c}%1$cint main(){%1$c%2$c/*%1$c%2$cle deuxieme commentaire%1$c%2$c*/%1$c%2$cfct();%1$c}%1$c";printf(a, 10, 9, 34, a);
 }
 int main(){
-	//le deuxieme commentaire
+	/*
+	le deuxieme commentaire
+	*/
 	fct();
 }
---
+++ C/Grace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+super se commentaire
+*/
+#define FT(x) int main(){FILE *fd = fopen(NAME, "w");fprintf(fd, RES, 10, 34, RES);}
+#define NAME "Grace_kid.c"
+#define RES "#include <stdio.h>%1$c/*%1$csuper se commentaire%1$c*/%1$c#define FT(x) int main(){FILE *fd = fopen(NAME, %2$cw%2$c);fprintf(fd, RES, 10, 34, RES);}%1$c#define NAME %2$cGrace_kid.c%2$c%1$c#define RES %2$c%3$s%2$c%1$c%1$cFT(xxxxxxxx)%1$c"
+
+FT(xxxxxxxx)
